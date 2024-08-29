e72eaba9dce53c66af58af2841a5829fdd1fbe84
AhammedNoyon <mdnoyon956993@gmail.com>
2024-08-30 03:17:37 +0600

    learn comparison in js

---
+++ comparison.js
@@ -0,0 +1,24 @@
+var num1 = 25;
+var num2 = 20;
+// console.log(num1 > num2);
+// console.log(num1 >= num2);
+
+// console.log(num1 < num2);
+// console.log(num1 <= num2);
+
+
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+// console.log(num1 !== num3);
+var num3 = "35";
+// console.log(num1===num3);
+
+// console.log(num1>num2 && num2<num3);
+// console.log(num1>num2 && num2>num3);
+
+// console.log(num1>num2 || num2<num3);
+// console.log(num1<num2 || num2>num3);
+
+
+
+
