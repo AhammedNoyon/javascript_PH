51ce32f291d949c80520b40824577b1d81817aa1
AhammedNoyon <mdnoyon956993@gmail.com>
2024-08-30 04:11:43 +0600

    Learn conditionals control statement

---
+++ if.js
@@ -0,0 +1,47 @@
+// Judgement even or odd number with if and else
+// var num = 5;
+
+// if(num/2==0){
+//   console.log('even');
+// }
+// else {
+//   console.log('odd');
+// }
+
+
+
+// Judgement negative positive zero with if else if else
+// var num1 = 0;
+// if(num1>0){
+//   console.log('this is positive value');
+// }
+// else if(num1 < 0){
+//   console.log('this is negative value');
+// }
+// else{
+//   console.log('this is zero value');
+// }
+
+
+
+// use logical operators && || with if else if else
+var penPrice = 10;
+var bookPrice = 200;
+
+// if(penPrice>bookPrice && penPrice==bookPrice){
+// console.log('I will buy this items')
+// }
+// else{
+//   console.log('Otherwise I do not buy this items')
+// }
+
+if(penPrice>bookPrice && penPrice==bookPrice){
+  console.log('I will buy this items')
+  }
+  else if(penPrice>15 || bookPrice>penPrice){
+    console.log('I will try buy in this items');
+  }
+  else{
+    console.log('Otherwise I do not buy this items')
+  } 
+
