8cf110a2bd7356f682f440bd2b9d59f92624a819
AhammedNoyon <mdnoyon956993@gmail.com>
2024-08-31 03:41:22 +0600

    Learn array related some concepts

---
+++ array.js
@@ -0,0 +1,53 @@
+// task 1
+/*
+let fruits = ["apple", "banana", "jackal", "mango", "lic", "coconut"];
+console.log(fruits);
+console.log(fruits[3]);
+fruits = fruits[2];
+console.log(fruits);
+fruits[2] = "jamb"
+console.log(fruits);
+*/
+
+// task 2
+/*
+let touristDestinations = ["bangladesh", "india", "japan"]
+console.log(touristDestinations);
+touristDestinations.push("southAfrica")
+console.log(touristDestinations);
+touristDestinations.push("northAfrica", "korea");
+console.log(touristDestinations);
+touristDestinations.pop();
+console.log(touristDestinations);
+*/
+
+// task 3
+/*
+let manyBooks = ["HTML", "CSS", "JAVASCRIPT", "TAILWIND", "REACT"]
+console.log(manyBooks.includes("JAVASCRIPT"));
+*/
+
+// task 4
+/*
+let text1 = "Bangladesh";
+let text2 = " is a land of river";
+let text = text1.concat(text2);
+console.log(text);
+console.log(Array.isArray(text));
+// or
+let books = ["math", "english", "physics", "biology", "chemistry"];
+let pen = ["redPen", "bluePen", "blackPen", "greenPen",];
+let booksPen = books.concat(pen);
+console.log(booksPen);
+console.log(Array.isArray(booksPen));
+*/
+
+// task 5
+/*
+let fontEnd = ["HTML", "CSS", "TAILWIND", "JAVASCRIPT", "REACT"];
+let backEnd = ["NODEJS", "MONGODB", "VUE"];
+let fullStake = fontEnd.concat(backEnd);
+console.log(fontEnd);
+console.log(backEnd);
+console.log(fullStake);
+*/
---
+++ module18.js
@@ -0,0 +1,66 @@
+// task 1
+/*
+let burger = 390;
+
+if (burger < 500){
+console.log('you will free eating burger');
+}
+else{
+  console.log('coke 30');
+}
+  */
+
+
+// task 2
+/*
+let weight,CentMiter;
+weight = 167;
+CentMiter = 65;
+const Miter = 100 / CentMiter;
+const height = Miter * Miter;
+const bmi = weight / height;
+if ( bmi < 18.5){
+  console.log('you are underweight');
+}
+else if ( bmi >= 18.5 && bmi <= 24.9){
+  console.log('you are normal');
+}
+else if (bmi >=25 && bmi <= 29.9){
+  console.log('you are overweight');
+}
+else{
+  console.log('you are obese');
+}
+*/
+
+
+// task 2
+// Determine Grade
+/*
+let Grade = 92;
+if (Grade >= 90 && Grade <=100){
+  console.log('golden A+');
+}
+else if (Grade >= 80 && Grade <= 89){
+  console.log('A+');
+}
+else if ( Grade >= 70 && Grade <= 79){
+  console.log('A');
+}
+else if ( Grade >= 60 && Grade <= 69){
+  console.log('A-');
+}
+else if ( Grade >= 50 && Grade <= 59){
+  console.log('B');
+}
+else if ( Grade >= 40 && Grade <= 49){
+  console.log('B-');
+}
+else if ( Grade >= 33 && Grade <= 39){
+  console.log('D only passed');
+}
+else{
+  console.log('Fail');
+}
+  */
+
